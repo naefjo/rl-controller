3cea8cda5cfd833e2fc05ee7fe3f1b21e1f66e4c
Joshua Näf <naefjo@ethz.ch>
2022-06-01 16:31:33 +0200

    fleshed out/fixed process dynamics

--- process_model.py
+++ process_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as plt
 
 
 # https://www.koreascience.or.kr/article/CFKO200333239336988.pdf
@@ -25,69 +26,115 @@ class InvertedPendulum:
         self.state = initial_state
         self.control_law = control_law
 
+        # Crazy lambda to get symbolic dynamics into an evaluable form
+        lambdify_sym = sp.lambdify(
+            sp.symbols("x1:5"),
+            self.dynamics_continuous_sym(),
+            modules="numpy",
+        )
+
+        self.dynamics_continuous = lambda x: np.squeeze(lambdify_sym(*x))
+
     def get_state(self):
         return self.state
 
     def dynamics_continuous_sym(self):
 
-        x1, x2, x3, x4 = sp.symbols("x1 x2 x3 x4")
+        x2, x3, x4 = sp.symbols("x2:5")
 
-        fraction = self.cart_mass + self.pendulum_mass * (np.sin(x3) ** 2)
+        fraction = self.cart_mass + self.pendulum_mass * (sp.sin(x3) ** 2)
 
         dynamics_vector_sym = sp.Matrix(
             [
-                [x2],
-                [
-                    (
-                        -self.pendulum_mass
-                        * self.gravity
-                        * np.sin(x3)
-                        * np.cos(x3)
-                        + self.pendulum_mass
-                        * self.pole_length
-                        * (x4 ** 2)
-                        * np.sin(x3)
-                        - self.friction_coeff_cart_ground * x2
-                        + self.control_law(self.state)
-                    )
-                    / fraction
-                ],
-                [x4],
-                [
-                    (
-                        -self.pendulum_mass
-                        * self.pole_length
-                        * (x4 ** 2)
-                        * np.sin(x3)
-                        * np.cos(x3)
-                        + self.friction_coeff_cart_ground * x2 * np.cos(x3)
-                        + (self.cart_mass + self.pendulum_mass)
-                        * self.gravity
-                        * np.sin(
-                            x3 - np.cos(x3 * self.control_law(self.state))
-                        )
-                    )
-                    / (self.pole_length * fraction)
-                ],
+                x2,
+                (
+                    -self.pendulum_mass
+                    * self.gravity
+                    * sp.sin(x3)
+                    * sp.cos(x3)
+                    + self.pendulum_mass
+                    * self.pole_length
+                    * (x4 ** 2)
+                    * sp.sin(x3)
+                    - self.friction_coeff_cart_ground * x2
+                    + self.control_law(self.state)
+                )
+                / fraction,
+                x4,
+                (
+                    -self.pendulum_mass
+                    * self.pole_length
+                    * (x4 ** 2)
+                    * sp.sin(x3)
+                    * sp.cos(x3)
+                    + self.friction_coeff_cart_ground * x2 * sp.cos(x3)
+                    + (self.cart_mass + self.pendulum_mass)
+                    * self.gravity
+                    * sp.sin(x3)
+                    - sp.cos(x3) * self.control_law(self.state)
+                )
+                / (self.pole_length * fraction),
             ]
         )
 
-        if not (
-            hasattr(self.__class__, "dynamics_continuous")
-            and callable(getattr(self.__class__, "dynamics_continuous"))
-        ):
-            self.dynamics_continuous = sp.lambdify(
-                (x1, x2, x3, x4), dynamics_vector_sym, modules="numpy"
-            )
+        # print(dynamics_vector_sym)
 
         return dynamics_vector_sym
 
-    def integrate_dynamics(self, timestep):
+    def integrate_dynamics_from_state(self, state, timestep):
         # simple RK4 integration
+        # print(state)
 
-        k1 = self.dynamics_continuous(self.state)
-        k2 = self.dynamics_continuous(self.state + timestep * k1 / 2)
-        k3 = self.dynamics_continuous(self.state + timestep * k2 / 2)
-        k4 = self.dynamics_continuous(self.state + timestep * k3)
+        k1 = self.dynamics_continuous(state)
+        # print("k1\n", k1)
+        k2 = self.dynamics_continuous(state + timestep * k1 / 2)
+        # print("k2\n", k2)
+        k3 = self.dynamics_continuous(state + timestep * k2 / 2)
+        # print("k3\n", k3)
+        k4 = self.dynamics_continuous(state + timestep * k3)
+        # print("k4\n", k4)
 
-        return self.state + timestep * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        new_state = state + timestep * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+        self.state = new_state
+        return new_state
+
+    def integrate_dynamics(self, timestep):
+        return self.integrate_dynamics_from_state(self.state, timestep)
+
+
+if __name__ == "__main__":
+    dyn = InvertedPendulum(
+        1.0,
+        2.0,
+        30.0,
+        0.4,
+        5.0,
+        np.array([0.0, 0.0, 0.01, 0.0]),
+        lambda x: x[0],
+    )
+
+    # print(dyn.dynamics_continuous([0.1, 0.2, 0.3, 0.4]))
+    iterations = 100
+    state_list = np.zeros((iterations, 4))
+    t = []
+    time_step = 0.1
+    for i in range(iterations):
+        t.append(i * time_step)
+        state_list[i, :] = dyn.integrate_dynamics(time_step)
+
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(t, state_list[:, 0])
+    axs[0, 0].set_title("x1")
+    axs[0, 1].plot(t, state_list[:, 1], "tab:orange")
+    axs[0, 1].set_title("x2")
+    axs[1, 0].plot(t, -state_list[:, 2], "tab:green")
+    axs[1, 0].set_title("x3")
+    axs[1, 1].plot(t, -state_list[:, 3], "tab:red")
+    axs[1, 1].set_title("x4")
+
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+
+    plt.savefig("out.png")
